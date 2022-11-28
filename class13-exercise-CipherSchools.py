9ab5efa50a68a8b8a8c27f7c53c2280cffdffe53
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2022-11-28 18:07:05 +0530

    --

---
+++ class13-exercise-CipherSchools.py
@@ -0,0 +1,9 @@
+# print these following Lines 
+# this is 11 double backslash
+# these are /\/\/\/\/\ mountains
+# he is awesome (use escape sequence instead of manual spaces)
+#1" \n \t \' (print this as an output)
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\//\\ mountains")
+print("he is\tawesome")
+print("\\\" \\n \\t \\\' ")
---
+++ class9-print-CipherSchools.py
@@ -0,0 +1,10 @@
+#correct ways-
+print("Hello world")
+print('hello world')
+print("hello 'world'")
+print('hello "world"')
+#incorrect -
+'''
+print("hello"world"")
+print('hello'world'')
+'''
